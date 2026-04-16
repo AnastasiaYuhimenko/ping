755aa5c36bdc75329edd7286e39c34fa91c9dc4f
Nastya <mail@yuhimenko-anastasia.ru>
2026-04-16 22:03:40 +0300

    dix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 cd /home/anastasia/backend
 export PATH="\$HOME/.cargo/bin:\$PATH"
 uv sync
+docker compose up -d
 sudo systemctl restart gunicorn || echo "gunicorn not found"
 echo "Deploy OK: \$(date)"
